5b64bedb04d03a165eacd1d72b8f83b63816b309
Manuel <carlosd99m@gmail.com>
2021-03-20 15:23:59 -0600

    finish app :rocket:

--- lib/main.dart
+++ lib/main.dart
@@ -1,21 +1,15 @@
 import 'package:flutter/material.dart';
 
 import 'home_page.dart';
- 
+
 void main() => runApp(MyApp());
- 
+
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      title: 'Material App',
-      home: Scaffold(
-        appBar: AppBar(
-          title: Text('Material App Bar'),
-        ),
-        body: HomePage()
-      ),
+      home: Scaffold(body: HomePage()),
     );
   }
 }
